c18d194f5a07f42da238295ba22b604735f475ed
ahmad <aalmadani111@gmail.com>
2023-06-09 20:39:10 +0200

    update models file and product name

--- product/models.py
+++ product/models.py
@@ -3,11 +3,12 @@ from datetime import datetime
 # Create your models here.
 
 
-class Item(models.Model):
-    name = models.CharField(max_length=50)
-    pro_details = models.TextField (max_length= 15000)
-    image = models.ImageField(upload_to='Items/', blank=True)
-    price = models.IntegerField()
+class Product(models.Model):
+    Pro_Name = models.CharField(max_length=50)
+    Pro_Descrebition = models.TextField (max_length= 15000)
+    Pro_Price = models.DecimalField( max_digits=5, decimal_places=2)
+    Pro_Cost = models.DecimalField (max_digits=5 , decimal_places=2)
+    Pro_date = models.DateTimeField(default=datetime.now , blank=True)
     category = models.CharField(max_length=50)
     def __str__(self):
-        return self.name
+        return self.Pro_Name
--- product/views.py
+++ product/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render , redirect
-from .models import Item
+from .models import Product
 # Create your views here.
 
 
 def New_item(request):
-    Product = Item.objects.all()
+    product = Product.objects.all()
     return render (request , 'product/home.html' , {'items':Product})
 
 def details_item(request ,item_id):
-    item = Item.objects.get(id=item_id)
+    item = Product.objects.get(id=item_id)
     return render (request , 'product/details.html' ,{'item':item})
